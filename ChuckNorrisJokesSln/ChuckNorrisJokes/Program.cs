751dd8070f2131af1af84b3ca5887440c001cdf8
Yizo-Yizo <56066066+Yizo-Yizo@users.noreply.github.com>
2019-10-02 15:12:12 +0200

    initial Chuck Norris commit

---
+++ ChuckNorrisJokesSln/ChuckNorrisJokes/Program.cs
@@ -0,0 +1,29 @@
+﻿using ChuckNorrisJokesLibrary;
+using System;
+using System.Threading.Tasks;
+
+namespace ChuckNorrisConsole
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            JokeGenerator jokeGenerator = new JokeGenerator();
+
+            string randomJokeString = await jokeGenerator.GetRandomJoke();
+
+            Console.WriteLine(randomJokeString);
+
+
+        //    JokeGenerator JokeGenerator = new JokeGenerator();
+
+         //   string[] categories = await JokeGenerator.GetCategories();
+
+        //    for (int index = 15; index > categories.Length; index--) 
+         //   {
+         //       return(categories[index]);
+        //    }
+            
+        }
+    }
+}
